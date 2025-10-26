2e25e68c43e39f9995a62f2449e429635cc0c710
VML8 <92667459+VML8@users.noreply.github.com>
2025-10-27 02:11:05 +0700

    Update requirements.txt encoding in setup.py
    Change encoding for reading requirements.txt to utf-8-sig.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 # Read requirements from requirements.txt
-with open("requirements.txt", "r", encoding="utf-8") as fh:
+with open("requirements.txt", "r", encoding="utf-8-sig") as fh:
     requirements = [line.strip() for line in fh if line.strip() and not line.startswith("#")]
 
 setup(
