ce52175c6232d430e69a5d937102caa332cda5fc
VML8 <92667459+VML8@users.noreply.github.com>
2025-11-22 07:28:42 +0700

    Display Object complete, now display 4 tiers of display. - Chapters - Chapter > Quests - Quest > Tasks/Rewards - Task / Reward object

--- module/view/display_data.py
+++ module/view/display_data.py
@@ -1,11 +1,9 @@
-from ast import List
+from rich import print
+import os, sys
 from os import path
 from rich.console import Console
 from rich.panel import Panel
 from rich.columns import Columns
-
-from ..controller.ftb_loader import ftb_loader
-from ..navigation.NavigationState import NavigationState
 
 def display_object(data, target_navigation):
     console = Console()
@@ -40,12 +38,19 @@ def display_object(data, target_navigation):
                     handle_container('Rewards', v)
                 else:
                     print_buffer.append(f"{k} : {v}")
-            panel = Panel.fit(f"{'\n'.join(print_buffer)}", title=f"{target_navigation[1]}", border_style="green", subtitle="Chapter")
+            panel = Panel.fit(f"{'\n'.join(print_buffer)}", title=f"{target_navigation[2]}", border_style="green", subtitle="Quests")
         case 4:
             # Need context check since it branches to either tasks or rewards.
             # if target_navigation[3] in 'tasks'. use navigate to tasks data at data[target_navigation[1]]['quests'][target_navigation[2]['tasks'][target_navigation[3]]]
+            _data_ref = data[target_navigation[1]]['quests'][target_navigation[2]]
+            _target_id = target_navigation[3]
 
-            pass
+            for container_name in ("tasks", "rewards"):
+                container = _data_ref.get(container_name, {})
+                if _target_id in container:
+                    for k,v in container[_target_id].items():
+                        print_buffer.append(f"{k} : {v}")  
+            panel = Panel.fit(f"{'\n'.join(print_buffer)}", title=f"{target_navigation[3]}", border_style="green", subtitle="Chapter")
                 
 
     console.print(panel)
@@ -53,10 +58,20 @@ def display_object(data, target_navigation):
         console.print(Columns(child_panel))
     
 
-if __name__ == "__main__":
-    cwd = path.abspath("V:\\Quest_Manager\\tests\\cwd_test")
-    data, data_quests, data_quest_rewards, data_quest_tasks, data_quest_task_container, data_quest_rewards_container = ftb_loader(cwd)
-    navigation = NavigationState()
-    navigation.go('welcome')
-    navigation.go('2B665C5D30FC7E86')
-    display_object(data, navigation.state)
+# if __name__ == "__main__":
+
+    # project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+    # if project_root not in sys.path:
+    #     sys.path.insert(0, project_root)
+        
+    # from module.controller.ftb_loader import ftb_loader
+    # from module.navigation.NavigationState import NavigationState
+
+    # cwd = path.abspath("V:\\Quest_Manager\\tests\\cwd_test")
+    # data, data_quests, data_quest_rewards, data_quest_tasks, data_quest_task_container, data_quest_rewards_container = ftb_loader(cwd)
+    # navigation = NavigationState()
+
+    # navigation.go('welcome')
+    # navigation.go('2B665C5D30FC7E86')
+    # navigation.go('65AC6680636196E9')
+    # display_object(data, navigation.state)
